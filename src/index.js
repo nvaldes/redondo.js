39bfe8bc2ff541aabcc03a807470b4a7cfa34ee5
Valdes, Noriel <nvaldes@c21stores.com>
2017-10-10 15:56:39 -0400

    added sonar icon with animation

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var THREE = require("three");
 window.THREE = THREE;
 require("three/examples/js/controls/OrbitControls");
+require("three/examples/js/renderers/CSS3DRenderer.js");
