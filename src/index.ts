e72e4198d13e806d603e561af760b51366dc9dd7
Valdes, Noriel <nvaldes@c21stores.com>
2017-10-10 14:48:55 -0400

    weirdness

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import * as THREE from "three";
 declare function require<T>(module: string): T;
 (window as any).THREE = THREE;
-require("three/examples/js/controls/OrbitControls");
+require("three/examples/js/renderers/CSS3DRenderer.js");
