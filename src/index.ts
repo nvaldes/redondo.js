d672ee1e09f1f5b0410e598d074e47a23db25e00
Valdes, Noriel <nvaldes@c21stores.com>
2017-10-25 10:45:10 -0400

    loading screen and mobile touch fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as THREE from "three";
+import * as Hammer from "hammerjs";
 declare function require<T>(module: string): T;
 (window as any).THREE = THREE;
+(window as any).Hammer = Hammer;
 require("three/examples/js/controls/OrbitControls.js");
 require("three/examples/js/renderers/CSS3DRenderer.js");
