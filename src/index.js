1b6165c61a1066de0b35159d713908b72433c2ca
Valdes, Noriel <nvaldes@c21stores.com>
2017-10-26 17:00:17 -0400

    refinements

--- src/index.js
+++ src/index.js
@@ -3,38 +3,27 @@ import BABYLON from 'babylonjs';
 import Hammer from 'hammerjs';
 
 window.redondo = function(config) {
+  window.redondo.killAllCards = function() {
+    document.querySelectorAll('.jm-360-outcards').forEach(function(e) {
+      e.remove();
+    });
+  }
   window.redondo.config = config;
   var canvas = document.querySelector(config.selector);
-  var engine = new BABYLON.Engine(canvas, true);
+  var engine = new BABYLON.Engine(canvas, true, { stencil: true });
 
   var createScene = function() {
-
-    // var products = [{
-    //     id: "0000-0000",
-    //     name: "foobar",
-    //     width: 2.5,
-    //     height: 8,
-    //     position: {
-    //         x: -31.75,
-    //         y: -1,
-    //         z: -20
-    //     },
-    //     tooltip: {
-    //         position: {
-    //             h: 1,
-    //             v: 1
-    //         },
-    //         content: 'DESIGNER\nMadonna and Child Statue\n$29,999.99'
-    //     }
-    // }];
-    // var productHitBoxes = {};
-
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
+    window.redondo.scene = scene;
     scene.zoomState = 1;
     scene.zoomSpeed = 0.05;
     scene.pause = true;
     scene.dragging = false;
+
+
+    // Create Assets Manager
+    var assetsManager = new BABYLON.AssetsManager(scene);
 
     // Create camera
     // var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(config.target.x, config.target.y, config.target.z), scene);
@@ -44,8 +33,8 @@ window.redondo = function(config) {
     camera.fov = scene.zoomState;
     camera.lowerRadiusLimit = 0.02;
     camera.upperRadiusLimit = 0.01;
-    camera.angularSensibilityX = 3000;
-    camera.angularSensibilityY = 2000;
+    camera.angularSensibilityX = config.sensitivityX;
+    camera.angularSensibilityY = config.sensitivityY;
     camera.panningSensibility = 0;
     scene.activeCamera = camera;
     scene.activeCamera.attachControl(canvas, true);
@@ -68,42 +57,90 @@ window.redondo = function(config) {
 
     // Create 360 view
     var env_mat = new BABYLON.StandardMaterial("Mat_Dome", scene);
-    var envtext = new BABYLON.Texture(config.background, scene);
-    env_mat.diffuseTexture = envtext;
-    env_mat.diffuseTexture.vScale = -1;
-    env_mat.emissiveTexture = envtext;
     env_mat.emissiveColor = new BABYLON.Color3(1, 1, 1);
     env_mat.backFaceCulling = false;
     dome.material = env_mat;
+    var env_texture = assetsManager.addTextureTask("image task", config.background);
+    env_texture.onSuccess = function(task) {
+      env_mat.diffuseTexture = task.texture;
+      env_mat.diffuseTexture.vScale = -1;
+      env_mat.emissiveTexture = task.texture;
+    }
 
-    var dev_mat = new BABYLON.StandardMaterial("mat_dev", scene);
-    var devtext = new BABYLON.Texture(config.devTexture, scene);
-    dev_mat.diffuseTexture = devtext;
-    dev_mat.diffuseTexture.vScale = -1;
-    dev_mat.emissiveTexture = devtext;
-    dev_mat.emissiveColor = new BABYLON.Color3(1, 1, 1);
-    dev_mat.backFaceCulling = false;
-    dev_mat.alpha = 0.55;
+    // var dev_texture = null;
+    // if (config.devTexture) {
+    //   var dev_texture = assetsManager.addTextureTask("image task", config.devTexture);
+    //   dev_texture.onSuccess = function(task) {
+    //     dev_texture = task.texture;
+    //   }
+    // }
+    var glow = [
+      new BABYLON.HighlightLayer('glow0', scene, config.glowOptions),
+      new BABYLON.HighlightLayer('glow1', scene, config.glowOptions),
+      new BABYLON.HighlightLayer('glow2', scene, config.glowOptions)
+    ];
+    glow.forEach((e) => {
+      e.innerGlow = false;
+    });
+    var alpha = 0;
+    scene.registerBeforeRender(() => {
+        alpha += config.twinkleSpeed;
+        if (alpha > (Math.PI)) {
+          alpha -= Math.PI
+        }
+        glow.forEach((e) => {
+          e.blurHorizontalSize = Math.sin(alpha) * config.glowSize;
+          e.blurVerticalSize = Math.sin(alpha) * config.glowSize;
+        });
+    });
 
     var linkNames = Object.keys(config.links);
-
+    window.meshes = [];
     for (var i = 0; i < linkNames.length; i++) {
       var curr = config.links[linkNames[i]];
+      if (curr.alpha == 0) {
+        continue;
+      }
       var mesh = BABYLON.MeshBuilder.CreateSphere(linkNames[i], {
         diameter: curr.diameter,
         sideOrientation: BABYLON.Mesh.DOUBLESIDE
       }, scene);
       mesh.actionManager = new BABYLON.ActionManager(scene);
       mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(e) {
-        alert(config.links[e.source.name].content);
+        scene.pause = false;
+        window.redondo.killAllCards();
+        var card = document.createElement('div');
+        card.className = 'jm-360-outcards';
+        card.id = 'card_' + e.source.name;
+        card.innerHTML = config.links[e.source.name].content;
+        card.style.top = e.pointerY + 'px';
+        card.style.left = e.pointerX + 'px';
+        var close = document.createElement('a');
+        close.href = '#'
+        close.className = 'jm-360-info-card-close';
+        close.innerHTML = '&times;'
+        close.addEventListener('click', function(e) {
+          e.preventDefault();
+          e.target.parentNode.remove();
+          window.redondo.scene.pause = !window.redondo.scene.pause;
+        });
+        card.appendChild(close);
+        canvas.parentNode.appendChild(card);
       }));
-      mesh.material = dev_mat;
+      mesh.material = new BABYLON.StandardMaterial("mat_dev", scene);
+      mesh.material.emissiveColor = new BABYLON.Color3.White();
+      mesh.material.alpha = curr.alpha;
       // debugger;
       // mesh.position = new BABYLON.Vector3(50, 0, 50);
-      mesh.position.x = config.domeRadius * Math.sin(curr.position.phi) * Math.cos(curr.position.theta);
-      mesh.position.y = config.domeRadius * Math.sin(curr.position.phi) * Math.sin(curr.position.theta);
-      mesh.position.z = config.domeRadius * Math.cos(curr.position.phi);
-      window.sampleMesh = mesh;
+      mesh.position.x = (config.domeRadius - curr.diameter) * Math.sin(curr.position.phi) * Math.cos(curr.position.theta);
+      mesh.position.y = (config.domeRadius - curr.diameter) * Math.cos(curr.position.phi);
+      mesh.position.z = (config.domeRadius - curr.diameter) * Math.sin(curr.position.phi) * Math.sin(curr.position.theta);
+      glow.forEach((e) => {
+        e.addMesh(mesh, BABYLON.Color3.White());
+      });
+      window.meshes.push(mesh);
+
+      assetsManager.load();
     }
     return scene;
   };
@@ -123,6 +160,10 @@ window.redondo = function(config) {
     }
     scene.render();
   });
+
+  // KillAllCards on touchstart/mousedown
+  canvas.addEventListener('touchstart', window.redondo.killAllCards);
+  canvas.addEventListener('mousedown', window.redondo.killAllCards);
 
   // Resize
   window.addEventListener("resize", function() {
